c6e27fe137dcbca7e45f1770d09fb53e9cdd291a
Altun <a.ozder@outlook.com>
2021-04-06 13:41:47 -0400

    Arrays

---
+++ src/Arrays/ArraySize.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class ArraySize {
+    static int[] numbers={1,225,231,4,675};
+    public static void main(String[] args) {
+        int size = numbers.length;
+        System.out.println("The size of array is:" + size);
+
+
+        String[] letter = {"z", "y", "x", "u", "v"};
+        int size1 = letter.length;
+        System.out.print("letter"+size1);
+
+
+    }
+}
