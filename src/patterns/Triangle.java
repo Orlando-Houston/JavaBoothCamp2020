aaf35b891e5bffb121148cee1173200daee56682
Altun <a.ozder@outlook.com>
2021-04-24 12:14:06 -0400

    patterns

---
+++ src/patterns/Triangle.java
@@ -0,0 +1,14 @@
+package patterns;
+
+public class Triangle {
+    public static void main(String[]args){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+        System.out.println("*******");
+
+    }
+}
